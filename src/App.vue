c2b79e21b8780191b0aea8cc4259ed86ee08380f
Jerry <fenturechance@gmail.com>
2021-08-03 14:53:59 +0800

    custom value of class

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
-    <h1>我是大標題</h1>
-    <h2>我是小標題</h2>
-    <button class="btn-blue">按鈕2</button>
+    <h1 class="text-gray">我是大標題</h1>
+    <h2 class="text-blue">我是小標題</h2>
+    <button class=" rounded-4xl bg-blue px-8 py-6">按鈕2</button>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      gray: '#eee',
+      blue: '#0af',
+    },
+    extend: {
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
   },
   variants: {
     extend: {},
